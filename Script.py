35bd72d2602774373a7a1994314a0b3df591ddb3
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2022-05-09 10:23:15 -0300

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,12 @@
+S = 'Hello world!'
+
+ # Does the string S start with 'Hello'?
+print(f'\n {S.startswith("Hello")}')
+
+ # Does the string S end with 'world'?
+print(f'\n {S.endswith("world")}')
+
+S1 = S.replace('world', 'people')
+
+ # How many occurrences of the word 'amazing' does string S1 have?
+print(f'\n {S1.count("amazing")}')
